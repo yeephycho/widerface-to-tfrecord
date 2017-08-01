ae945f4ce55f89320a3d849e6d57535f43840ca3
yeephycho <yeephycho@github.com>
2017-08-01 16:59:24 +0800

    deleted:    output/train.tfrecord 	modified:   widerface_To_TFRecord.py

--- widerface_To_TFRecord.py
+++ widerface_To_TFRecord.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 import numpy
 import cv2
 import os
+import hashlib
 
 from utils import dataset_util
 
@@ -19,7 +20,7 @@ def parse_example(f):
   width = None # Image width
   filename = None # Filename of the image. Empty if image is not from file
   encoded_image_data = None # Encoded image bytes
-  image_format = b'jpg' # b'jpeg' or b'png'
+  image_format = b'jpeg' # b'jpeg' or b'png'
 
   xmins = [] # List of normalized left x coordinates in bounding box (1 per box)
   xmaxs = [] # List of normalized right x coordinates in bounding box (1 per box)
@@ -27,6 +28,10 @@ def parse_example(f):
   ymaxs = [] # List of normalized bottom y coordinates in bounding box (1 per box)
   classes_text = [] # List of string class name of bounding box (1 per box)
   classes = [] # List of integer class id of bounding box (1 per box)
+  poses = []
+  truncated = []
+  difficult_obj = []
+
 
 
   filename = f.readline().rstrip()
@@ -36,22 +41,32 @@ def parse_example(f):
   image_raw = cv2.imread(filepath)
 
   encoded_image_data = open(filepath).read()
+  key = hashlib.sha256(encoded_image_data).hexdigest()
+
   height, width, channel = image_raw.shape
   print("height is %d, width is %d, channel is %d" % (height, width, channel))
 
   face_num = int(f.readline().rstrip())
-  print(face_num)
+  valid_face_num = 0
 
   for i in range(face_num):
     annot = f.readline().rstrip().split()
     # WIDER FACE DATASET CONTAINS SOME ANNOTATIONS WHAT EXCEEDS THE IMAGE BOUNDARY
-    xmins.append( max(0, (float(annot[0]) / width) ) )
-    ymins.append( max(0, (float(annot[1]) / height) ) )
-    xmaxs.append( min(1, ((float(annot[0]) + float(annot[2])) / width) ) )
-    ymaxs.append( min(1, ((float(annot[1]) + float(annot[3])) / height) ) )
-    classes_text.append('face')
-    classes.append(1)
-    print(xmins[i], ymins[i], xmaxs[i], ymaxs[i], classes_text[i], classes[i])
+    if(float(annot[2]) > 25.0):
+        if(float(annot[3]) > 30.0):
+            xmins.append( max(0.005, (float(annot[0]) / width) ) )
+            ymins.append( max(0.005, (float(annot[1]) / height) ) )
+            xmaxs.append( min(0.995, ((float(annot[0]) + float(annot[2])) / width) ) )
+            ymaxs.append( min(0.995, ((float(annot[1]) + float(annot[3])) / height) ) )
+            classes_text.append('face')
+            classes.append(1)
+            poses.append("front".encode('utf8'))
+            truncated.append(int(0))
+            print(xmins[-1], ymins[-1], xmaxs[-1], ymaxs[-1], classes_text[-1], classes[-1])
+            valid_face_num += 1;
+
+  print("Face Number is %d" % face_num)
+  print("Valid face number is %d" % valid_face_num)
 
 
   tf_example = tf.train.Example(features=tf.train.Features(feature={
@@ -59,17 +74,22 @@ def parse_example(f):
     'image/width': dataset_util.int64_feature(int(width)),
     'image/filename': dataset_util.bytes_feature(filename),
     'image/source_id': dataset_util.bytes_feature(filename),
+    'image/key/sha256': dataset_util.bytes_feature(key.encode('utf8')),
     'image/encoded': dataset_util.bytes_feature(encoded_image_data),
-    'image/format': dataset_util.bytes_feature(image_format),
+    'image/format': dataset_util.bytes_feature('jpeg'.encode('utf8')),
     'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
     'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
     'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
     'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
     'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
     'image/object/class/label': dataset_util.int64_list_feature(classes),
+    'image/object/difficult': dataset_util.int64_list_feature(int(0)),
+    'image/object/truncated': dataset_util.int64_list_feature(truncated),
+    'image/object/view': dataset_util.bytes_list_feature(poses),
     }))
 
-  return tf_example
+
+  return valid_face_num, tf_example
 
 
 def main(unused_argv):
@@ -77,12 +97,22 @@ def main(unused_argv):
   writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
 
 # WIDER FACE DATASET ANNOTATED 12880 IMAGES
+  valid_image_num = 0
+  invalid_image_num = 0
   for image_idx in range(12880):
-    print(image_idx)
-    tf_example = parse_example(f)
-    writer.write(tf_example.SerializeToString())
-
+    print("image idx is %d" % image_idx)
+    valid_face_number, tf_example = parse_example(f)
+    if(valid_face_number != 0):
+      writer.write(tf_example.SerializeToString())
+      valid_image_num += 1
+    else:
+      invalid_image_num += 1
+      print("Pass!")
   writer.close()
+
+  print("Valid image number is %d" % valid_image_num)
+  print("Invalid image number is %d" % invalid_image_num)
+
 
 
 if __name__ == '__main__':
